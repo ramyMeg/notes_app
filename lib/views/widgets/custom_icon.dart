4bb9fb986652ed32bcce4a1dc8b6392bf99ae471
ramyMeg <ramymegall@gmail.com>
2025-03-31 16:11:19 +0200

    custom for appbar - search icon -Notes viewbody

---
+++ lib/views/widgets/custom_icon.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 50,
+      decoration: BoxDecoration(
+        color: Colors.white.withAlpha(50),
+        borderRadius: BorderRadius.circular(16),
+      ),
+
+      child: Center(child: Icon(Icons.search, size: 30)),
+    );
+  }
+}
